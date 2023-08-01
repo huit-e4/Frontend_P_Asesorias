42b36185849d096d3445f38b71c9fc325fa8b194
JuanMMP99 <84467177+JuanMMP99@users.noreply.github.com>
2023-08-01 12:11:30 -0600

    Se creo la interfaz del perfil del Estudiante

--- src/app/components/estudiantescomponents/perfil-estudiante/perfil-estudiante.component.ts
+++ src/app/components/estudiantescomponents/perfil-estudiante/perfil-estudiante.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-perfil-estudiante',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil-estudiante.component.css']
 })
 export class PerfilEstudianteComponent {
+  ngOnInit(): void {
+    this.getUser();
+    //this.loadData();
+  }
+
+
+
+
+
+  constructor(private userS: UserService, private http: HttpClient) {
+  }
+
+  
+
+    //loadData(){
+    // Llamar a la función getExperts() del servicio
+    //this.userS.getPerfil().subscribe(
+    //(experts: any) => {
+    // Mostrar los datos en la consola
+    //console.log('Datos de expertos:', experts);
+    //},
+    // (error) => {
+    //console.error('Error al obtener los expertos:', error);
+    // }
+    //);
+
+    userD:any;;
+    getUser(){
+      const userL=JSON.parse(localStorage.getItem('user')||'[]');
+      this.userD=userL;
+      console.log(this.userD);
+  
+      
+    }
 
 }
